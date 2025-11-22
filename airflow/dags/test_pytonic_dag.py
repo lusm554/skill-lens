5208e4bd3c3d3a8d61eeb1a98ffdf6edc19dd6f7
lusm <loveyousomuch554@gmail.com>
2025-11-22 15:35:31 +0300

    add example of ETL tasks

--- airflow/dags/test_pytonic_dag.py
+++ airflow/dags/test_pytonic_dag.py
@@ -16,10 +16,44 @@ def own_tutorial_taskflow_api():
     def hello():
         """
         ### Prints "Hello world!"
-        Hui
         """
         print('Hello World!')
+
+    @task()
+    def extract():
+        """
+        # Extract task
+        Extracts data sample
+        """
+        extract_example = {
+            "payload": [1, 0, 1, 0]
+        }
+        return extract_example
+
+    @task(multiple_outputs=False)
+    def transform(extract_example: dict):
+        """
+        # Transform task
+        Transforms sample data
+        """
+        decoded_msg = ""
+        for bit in extract_example['payload']:
+            if bit==1: decoded_msg+='a'
+            else: decoded_msg+='b'
+        return {"decoded_msg": decoded_msg}
+
+    @task()
+    def load(decoded_msg):
+        """
+        # Load task
+        Loads data to storage, stdout
+        """
+        print(f"Decoded message: {decoded_msg}")
+
     hello()
+    extract_example = extract()
+    decoded_msg = transform(extract_example)
+    load(decoded_msg)
 
 # Call function for register dag in airflow
 own_tutorial_taskflow_api()
