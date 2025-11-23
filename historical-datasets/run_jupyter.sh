f3b80d8a239028a574b8645d5c5bb9b9cca951a3
lusm <loveyousomuch554@gmail.com>
2025-11-23 15:40:26 +0300

    change notebook dir to skill lens root dir

--- historical-datasets/run_jupyter.sh
+++ historical-datasets/run_jupyter.sh
@@ -3,4 +3,4 @@ source .venv/bin/activate
 which python3
 python3 -V
 python3 -m ipykernel install --user --name .venv --display-name "Python3.14 historical-datasets"
-jupyter lab 
+jupyter lab --notebook-dir="../"
